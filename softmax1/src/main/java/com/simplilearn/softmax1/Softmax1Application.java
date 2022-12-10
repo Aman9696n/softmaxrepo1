7ee43afd94751233a503a1e6bf261b30602f58d1
Aman9696n <amanmishra9696@gmail.com>
2022-12-10 17:28:39 +0000

    this is spring boot project

---
+++ softmax1/src/main/java/com/simplilearn/softmax1/Softmax1Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmax1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Softmax1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Softmax1Application.class, args);
+	}
+
+}
---
+++ softmax1/src/test/java/com/simplilearn/softmax1/Softmax1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmax1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Softmax1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
